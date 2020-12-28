a29ce754f9527f08364615f43b7ec4cb38fc1ca8
Contemplat0r <uldo.forme@gmail.com>
2020-12-28 15:00:40 +0200

    Backend now working (without received symbols caching).

--- app/main/controller/pyex.py
+++ app/main/controller/pyex.py
@@ -13,15 +13,10 @@ api = Namespace('PyEXSymbols', description="iexCloud symbols operations")
 @api.route('/symbols')
 class PyEXSymbols(Resource):
 
-    #@api.doc('Receive iexCloud symbols', body=_pyex_data)
-    @api.doc('Receive iexCloud symbols')
     @api.response(200, "iexCloud symbols received")
     def post(self, *args, **kwargs):
         """Receive iexCloud symbols that math pattern """
-        print("PyEXSymbols, request:\n", request)
-        print("PyEXSymbols, dir(request):\n", dir(request))
         data = request.json
-        print("PyEXSymbols.post, data:\n", data)
         symbols_pattern = data['symbols_pattern']
         return receive_pyex_symbols(symbols_pattern)
 
--- app/main/service/pyex.py
+++ app/main/service/pyex.py
@@ -6,9 +6,6 @@ iex_cloud_client = pyEX.Client(
     )
 
 def receive_pyex_symbols(symbols_pattern):
-    print("receive_pyex_symbol, symbols_pattern: ", symbols_pattern)
     all_symbols = [symbol_dict['symbol'] for symbol_dict in iex_cloud_client.symbols()]
     selected_symbols = [symbol for symbol in all_symbols if symbol.startswith(symbols_pattern)]
-    print("receive_pyex_symbols, len(all_symbols): ", len(all_symbols))
-    print("receive_pyex_symbols, len(selected_symbols): ", len(selected_symbols))
     return selected_symbols
